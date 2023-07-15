a58e53bbcf25da660cd72c204af16e95015fbb2a
jalcumb <101678531+jacobalcu@users.noreply.github.com>
2023-07-15 15:35:44 -0400

    Don't remember what I changed

--- index.js
+++ index.js
@@ -5,3 +5,19 @@ for (let i = 0; i < 16 * 16; i++) {
   div.classList.add("inner");
   outerDiv.appendChild(div);
 }
+
+let divs = document.querySelectorAll(".inner");
+divs.forEach((div) => {
+  div.addEventListener("mousemove", (e) => {
+    e.target.classList.add("triggered");
+  });
+});
+
+let resetFunc = () => {
+  divs.forEach((div) => {
+    div.classList.remove("triggered");
+  });
+};
+
+let button = document.querySelector(".reset");
+button.addEventListener("click", resetFunc);
